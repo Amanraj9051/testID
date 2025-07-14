2c2e413267ca7c713da23acaabd69834d848e23f
Amanraj9051 <142516704+Amanraj9051@users.noreply.github.com>
2025-07-14 13:16:03 +0530

    Update EmailService.java

--- src/p1/EmailService.java
+++ src/p1/EmailService.java
@@ -5,7 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class EmailService {
     public void sendEmail(String msg)
     {
-    System.out.println("Email Sent " + msg);
+    System.out.println("Email Sent by ..." + msg);
     }
 
 
